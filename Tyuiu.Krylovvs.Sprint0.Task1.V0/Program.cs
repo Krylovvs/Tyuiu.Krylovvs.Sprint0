7c9f4b26b690105c8111b3c21a07f1e6522439dd
huh <matemat230@gmail.com>
2026-09-10 16:05:56 +0500

    Task1

---
+++ Tyuiu.Krylovvs.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("HUH\n");
+
+        string Name = "SirHUH";
+        int Age = 76;
+
+        Console.WriteLine($"Катаю под никнеймом {Name}, мне {Age} лет");
+
+        Console.ReadKey();
+
+    }
+}
