d6cb8a39efc4ab14a348f7d6ddd5469c2bbfe192
huh <matemat230@gmail.com>
2026-09-18 01:10:53 +0500

    Task5

---
+++ Tyuiu.Krylovvs.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿namespace Tyuiu.Krylovvs.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("не делай так");
+
+            return a / b;
+        }
+    }
+}
