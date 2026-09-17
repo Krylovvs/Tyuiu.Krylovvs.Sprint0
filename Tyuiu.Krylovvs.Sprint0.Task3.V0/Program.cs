1d4a83fb99c7b3250641bb5fef0b5999a8f8d4ac
huh <matemat230@gmail.com>
2026-09-18 00:03:58 +0500

    Con

---
+++ Tyuiu.Krylovvs.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.Krylovvs.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.Krylovvs.Sprint0.Task3.V0
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+
+}
