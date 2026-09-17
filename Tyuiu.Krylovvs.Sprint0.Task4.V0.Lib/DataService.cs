bbd25b0abcd14c0b81362a8767a1348146dc9386
huh <matemat230@gmail.com>
2026-09-18 00:51:23 +0500

    Task3

---
+++ Tyuiu.Krylovvs.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿namespace Tyuiu.Krylovvs.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static Num Addition(Num a, Num b) {
+            return a + b;
+        }
+        public static Num Subtraction(Num a, Num b) {
+            return a - b;
+        }
+        public static Num Multiplication(Num a, Num b) {
+            return a * b;
+        }
+        public static Num Division(Num a, Num b) {
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.Krylovvs.Sprint0.Task4.V0.Lib/ItsStructForWhat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.Krylovvs.Sprint0.Task4.V0.Lib
+{
+    public readonly record struct Num(int Value)
+    {
+
+        public static implicit operator Num(int Value)
+            => new Num(Value);
+        public static Num operator +(Num a, Num b) =>
+            new(a.Value + b.Value);
+        public static Num operator -(Num a, Num b) =>
+            new(a.Value - b.Value);
+        public static Num operator *(Num a, Num b) =>
+            new(a.Value * b.Value);
+        public static Num operator /(Num a, Num b)
+        {
+            if (b.Value == 0)
+                throw new DivideByZeroException();
+
+            return new Num(a.Value / b.Value);
+        }
+    }
+}
